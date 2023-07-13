ee7330891ffaf318c31999d1c510911d015de5cc
Akshay-Shingala <akshay.shingala@gmail.com>
2023-07-13 18:44:31 +0530

    jason data

--- json data send/consumer.py
+++ json data send/consumer.py
@@ -3,7 +3,7 @@ import json
 import msgpack
 # To consume latest messages and auto-commit offsets
 # give the topic name a unique and give the server port by default i will be :9092
-consumer = KafkaConsumer('my_topic', bootstrap_servers=['127.0.0.1:9092'], group_id='my-group')
+consumer = KafkaConsumer('my_topic', bootstrap_servers=['127.0.0.1:9092'],)
 
 
 #get data from consumer this loop executes continuously
@@ -11,6 +11,9 @@ for message in consumer:
     try:
         # print data from consumer message data get in message.value
         print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key, message.value))
+        # by default it show data in bytes formet or say encripted formet
+        # to read that data we need to unpack that message
+        print("%s:%d:%d: key=%s value=%s" % (message.topic, message.partition, message.offset, message.key,msgpack.loads( message.value)))
     except Exception as e:
         print(e,"except")
  
--- json data send/producer.py
+++ json data send/producer.py
@@ -3,28 +3,8 @@ from kafka.errors import KafkaError
 import json
 import msgpack  
 import logging
-# Configure the logger
-logging.basicConfig(level=logging.ERROR)  # Set the desired log level
-
-# Create a logger instance
-log = logging.getLogger(__name__)
-
-producer = KafkaProducer(bootstrap_servers=['127.0.0.1:9092'])
-
-# Asynchronous by default
-future = producer.send('my_topic', b'send data')
-
-# Block for 'synchronous' sends
-try:
-    record_metadata = future.get(timeout=10)
-except KafkaError:
-    # Decide what to do if produce request failed...
-    log.exception()
-    pass
-
-print (record_metadata.topic)
-print (record_metadata.partition)
-print (record_metadata.offset)
-
+# producer = KafkaProducer(bootstrap_servers=['127.0.0.1:9092'])
+producer = KafkaProducer(bootstrap_servers=['127.0.0.1:9092'],value_serializer=msgpack.dumps)
+producer.send('my_topic', {'key': 'value'})
 producer.flush()
 producer.close()
